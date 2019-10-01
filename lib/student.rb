a2c8de9be04829a6d93a8d3ebfe306f2287fc48e
Dean L Betty <deanlloyd64@gmail.com>
2019-10-01 19:03:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
+    binding.pry
     students_array.each do |student|
       student.each do |key, value|
         self.send("#{key}=", value)
