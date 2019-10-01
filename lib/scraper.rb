d36a7da2b400986b09590d34864c41b251cf1219
Dean L Betty <deanlloyd64@gmail.com>
2019-10-01 15:14:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    student_index = Nokogiri::HTML(open(index_url))
+    page = Nokogiri::HTML(open(index_url))
     binding.pry
     student_index.css("div.student-card")
   end
