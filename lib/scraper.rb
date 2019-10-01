f56ca31e0681f8da14a8278b9db0b3b2a9db12c3
Dean L Betty <deanlloyd64@gmail.com>
2019-10-01 16:04:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,13 +8,13 @@ class Scraper
     page = Nokogiri::HTML(open(index_url))
     student_index = page.css("div.student-card")
     students = student_index.collect do |student|
+      binding.pry
       {
         :name => student_index.css("h4.student-name").text,
         :location => student_index.css("p.student-location").text,
         :profile_url => student_index.css("a").attribute("href").text
       }
     end
-    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
