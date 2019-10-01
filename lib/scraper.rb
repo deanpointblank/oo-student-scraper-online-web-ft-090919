1a1fc45edbb7b539ae0a86a5082a01ba30e394ba
Dean L Betty <deanlloyd64@gmail.com>
2019-10-01 17:51:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,6 @@ class Scraper
       page.css("div.social-icon-container a").each do |social|
         case text = social.attribute("href").text
         binding.pry
-          when 
         # profile[:twitter] = page.css("div.social-icon-container a")[0].attribute("href").text
         # profile[:linkedin] = page.css("div.social-icon-container a")[1].attribute("href").text
         # profile[:github] = page.css("div.social-icon-container a")[2].attribute("href").text
